ed7447b5a55ef529e48e6e6e91bc1eefd26ebf8f
audisiogaston <audisiogaston@gmail.com>
2022-11-22 01:04:36 -0300

    borro todo

--- js/stock.js
+++
@@ -1,9 +0,0 @@
-
-let libros = [
-    {id:1,nombre:'Heidi',genero:'Drama',precio:5000,cantidad:1,img:'src/libros/heidi.svg'},
-    {id:2,nombre:'Orgullo y Prejuicio',genero:'Drama',precio:6000,cantidad:1,img:'src/libros/orgullosvg.svg'},
-    {id:3,nombre:'Frankenstein',genero:'Terror',precio:4000,cantidad:1,img:'src/libros/franken.svg'},
-    {id:4,nombre:'El libro de la Selva',genero:'Aventura',precio:2000,cantidad:1,img:'src/libros/libroselva.svg'},
-    {id:5,nombre:'1984',genero:'Ciencia Ficción',precio:3000,cantidad:1,img:'src/libros/1984.svg'}
-    
-]
